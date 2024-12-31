d66d88b1e0624793d12a86ee3d26a5ef606c3c81
KullanderSebastian <kullander.sebastian@gmail.com>
2024-12-31 10:44:58 +0100

    Removed unused/unnecessary files, created some basic components

---
+++ src/app/Layout/Header.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Header() {
+    return (
+        <div className="w-full bg-slate-400">
+            <h1>Header Color shade generator</h1>
+        </div>
+    )
+}
---
+++ src/app/components/ShadeGenerator.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function ShadeGenerator() {
+    return (
+        <h1>Hello shade generator</h1>
+    )
+}
